a3b1b5eac65085947d61d569804d959a75015ba9
funterminal <imsmallguyintheground@gmail.com>
2025-05-02 19:11:08 +0600

    Update splitpaths.sh

--- splitpaths.sh
+++ splitpaths.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 splitpaths() {
-    for path in "$@"; do
-        if [ -z "$path" ]; then
-            continue
-        fi
-
-        echo "$path" | awk -F/ '{ for (i = 1; i <= NF; i++) print $i }'
-    done
+    if [ "$#" -eq 0 ]; then
+        while IFS= read -r line; do
+            [ -z "$line" ] && continue
+            echo "$line" | awk -F/ '{ for (i = 1; i <= NF; i++) if ($i != "") print $i }'
+        done
+    else
+        for path in "$@"; do
+            [ -z "$path" ] && continue
+            echo "$path" | awk -F/ '{ for (i = 1; i <= NF; i++) if ($i != "") print $i }'
+        done
+    fi
 }
 
 splitpaths "$@"
